709d6f9904546053756c1bd081f53f8ab7eb723d
Pablo Dias <pablo.dias.dev@gmail.com>
2022-06-29 11:43:46 -0300

    First commit

---
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -0,0 +1,78 @@
+package com.algaworks.osworks.domain.model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.algaworks.osworks.domain.ValidationGroups;
+
+@Entity
+public class Cliente {
+	@NotNull(groups = ValidationGroups.ClienteId.class)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(max=60)
+	@NotBlank
+	private String nome;
+		
+	@Email
+	@Size(max=255)
+	@NotBlank
+	private String email;
+		
+	@Size(max=20)
+	@NotBlank
+	private String telefone;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
